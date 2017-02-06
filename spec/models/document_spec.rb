225e9acf593fad739ded48cbaf7b914889691253
Adam Gorbahn <adamgorbahn@hotmail.com>
2017-02-06 22:47:24 +0000

    update the document model with invalid without project

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -9,6 +9,10 @@ describe Document do
     expect( FactoryGirl.build(:document, title: nil) ).not_to be_valid
   end
 
+  it "returns a Document title as a string" do
+    expect( FactoryGirl.create(:document, title: "Bob").title ).to eq( "Bob" )
+  end
+  
   it "returns a historical event title as a string" do
     expect( FactoryGirl.create(:document, title: "Bob").title ).to eq( "Bob" )
   end
@@ -16,6 +20,10 @@ describe Document do
   it "belongs to project" do
     assc = described_class.reflect_on_association(:project)
     expect( assc.macro ).to eq :belongs_to
+  end
+  
+  it "is invalid without a project id" do
+    expect{ FactoryGirl.create( :document, project: nil ) }.to raise_error(ActiveRecord::RecordInvalid)
   end
   
 end
