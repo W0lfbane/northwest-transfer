8b495cecb29b4df453f33cffb9027c74685dd8ef
Adam Gorbahn <adamgorbahn@hotmail.com>
2017-02-10 23:50:03 +0000

    test for no params for calendar

--- spec/requests/custom_route_spec.rb
+++ spec/requests/custom_route_spec.rb
@@ -63,6 +63,11 @@ RSpec.describe "Custom Routes", :type => :request do
         get projects_calendar_path,  params: { projects: nil }
         expect(response).to have_http_status(:success)
       end
+      
+      it "Pass with no params" do
+        get projects_calendar_path
+        expect(response).to have_http_status(:success)
+      end
     end
   end
   
