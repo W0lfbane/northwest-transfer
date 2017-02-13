994d318d74ef69c2c6e9d8a0de94092a9bf0b610
Adam Gorbahn <adamgorbahn@hotmail.com>
2017-02-13 23:53:31 +0000

    add tests for project edit page

--- spec/requests/custom_route_spec.rb
+++ spec/requests/custom_route_spec.rb
@@ -119,4 +119,31 @@ RSpec.describe "Custom Routes", :type => :request do
     end
   end
   
+  describe "Edit project step path for projects#edit" do
+    
+    before :each do
+        @admin = FactoryGirl.create(:admin)
+        @project = FactoryGirl.create(:project)
+        sign_in(@admin)
+        @admin.add_role :leader, @project
+        @admin.projects << @project
+    end
+      
+    it "get the project edit page by the project id and pending" do
+        get edit_project_step_path(@project)
+        expect(response).to have_http_status(:success)
+    end
+    
+    it "redirects if wrong step is passed in" do
+        get edit_project_step_path(@project, step: :completed) 
+        expect(response).to have_http_status(:redirect)
+    end
+    
+    it "renders show template" do
+        get edit_project_step_path(@project)
+        expect(response).to render_template :edit
+    end
+    
+  end
+  
 end
