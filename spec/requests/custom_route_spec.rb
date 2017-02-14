7ef429db7b73cee21a39b66d31ca9292827bc217
Adam Gorbahn <adamgorbahn@hotmail.com>
2017-02-14 18:17:12 +0000

    add test for admin and user

--- spec/requests/custom_route_spec.rb
+++ spec/requests/custom_route_spec.rb
@@ -120,28 +120,54 @@ RSpec.describe "Custom Routes", :type => :request do
   end
   
   describe "Edit project step path for projects#edit" do
-    
-    before :each do
-        @admin = FactoryGirl.create(:admin)
-        @project = FactoryGirl.create(:project)
-        sign_in(@admin)
-        @admin.add_role :leader, @project
-        @admin.projects << @project
-    end
+    context "logged in as admin" do
+      before :each do
+          @admin = FactoryGirl.create(:admin)
+          @project = FactoryGirl.create(:project)
+          sign_in(@admin)
+          @admin.add_role :leader, @project
+          @admin.projects << @project
+      end
+        
+      it "get the project edit page by the project id and pending" do
+          get edit_project_step_path(@project)
+          expect(response).to have_http_status(:success)
+      end
       
-    it "get the project edit page by the project id and pending" do
-        get edit_project_step_path(@project)
-        expect(response).to have_http_status(:success)
+      it "redirects if wrong step is passed in" do
+          get edit_project_step_path(@project, step: :completed) 
+          expect(response).to have_http_status(:redirect)
+      end
+      
+      it "renders show template" do
+          get edit_project_step_path(@project)
+          expect(response).to render_template :edit
+      end
     end
     
-    it "redirects if wrong step is passed in" do
-        get edit_project_step_path(@project, step: :completed) 
-        expect(response).to have_http_status(:redirect)
-    end
-    
-    it "renders show template" do
-        get edit_project_step_path(@project)
-        expect(response).to render_template :edit
+    context "logged in as user" do
+      before :each do
+          @user = FactoryGirl.create(:user)
+          @project = FactoryGirl.create(:project)
+          sign_in(@user)
+          @user.add_role :leader, @project
+          @user.projects << @project
+      end
+        
+      it "get the project edit page by the project id and pending" do
+          get edit_project_step_path(@project)
+          expect(response).to have_http_status(:success)
+      end
+      
+      it "redirects if wrong step is passed in" do
+          get edit_project_step_path(@project, step: :completed) 
+          expect(response).to have_http_status(:redirect)
+      end
+      
+      it "renders show template" do
+          get edit_project_step_path(@project)
+          expect(response).to render_template :edit
+      end
     end
     
   end
