bdf405eb668da92efb76370846bb44ed633b4c2c
Ewa <kocica@gmail.com>
2017-02-17 21:35:00 +0000

    Add respond_to method to resource roles helper to make it possible to check if an object responds to a method

--- app/models/helpers/resource_roles_helper.rb
+++ app/models/helpers/resource_roles_helper.rb
@@ -13,6 +13,18 @@ module Helpers::ResourceRolesHelper
             super
         end
     end
+    
+    #allows objects that implement above ghost method to return true for #respond_to and #method for generated methods
+    def respond_to_missing?(method, *)
+      method_name = method.to_s
+      role_name = method_name.singularize
+      method_is_role = method_name[/[a-zA-Z]+/] == method_name 
+      if (self.respond_to?(:roles) && self.class.respond_to?(:find_roles))
+        show_roles(self.class).include?(role_name) && method_is_role
+      else 
+        super
+      end
+    end
 
     # Returns and array of existing roles on self
     def show_roles(object = self)
@@ -22,5 +34,7 @@ module Helpers::ResourceRolesHelper
             @roles ||= object.find_roles.pluck(:name)
         end
     end
+    
+
 
 end
