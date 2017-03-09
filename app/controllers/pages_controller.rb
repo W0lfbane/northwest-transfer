c66ab045a38f339e9182916e191cdfda1d7a3045
Ewa <kocica@gmail.com>
2017-03-09 00:46:23 +0000

    refactor fix into concern

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,20 @@
 class PagesController < ApplicationController
+    include Concerns::String::SqlFilters
+    
+    before_action :authorize_page
+
     def show
-        authorize :page
-        if(!params[:resources].nil?)
-            params[:resources].each do |key, value| 
-                value_to_set = is_invalid?(value) ? nil : eval(value)
-                instance_variable_set("@#{key}", policy_scope(value_to_set) ) unless value_to_set.nil?
-            end
-        end
+        params[:resources].each do |key, value|
+            value_to_set = destructive_transaction?(value) ? nil : eval(value)
+            instance_variable_set( "@#{key}", policy_scope(value_to_set) ) unless value_to_set.nil?
+        end unless(params[:resources].nil?)
+
         render template: "pages/#{params[:page]}"
     end
     
-private
-    #returns true if input string does not follow pattern of: Modelname or Modelname.find/where/order/limit
-    #note: the where query is currently limited to only single hash conditions
-    def is_invalid? (value)
-        value !~ /(?:\A[A-Z][a-z]+)(?:(\.)?(?(1)(?:(?:limit|find|order|where)\(\w*:?\s?:?'?\w*'?\))))*$/
-    end
+    private
+
+        def authorize_page
+            authorize :page
+        end
 end
