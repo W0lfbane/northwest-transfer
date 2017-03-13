c780eeec157442a10d08659ed7e45e1419aae8b3
Ewa <kocica@gmail.com>
2017-03-13 20:48:44 +0000

    Add migration removing notes columns from tasks, projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313201256) do
+ActiveRecord::Schema.define(version: 20170313204727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(version: 20170313201256) do
     t.datetime "start_date",                               null: false
     t.datetime "completion_date"
     t.datetime "estimated_completion_date",                null: false
-    t.text     "notes"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
     t.string   "resource_state"
@@ -112,7 +111,6 @@ ActiveRecord::Schema.define(version: 20170313201256) do
     t.integer  "project_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.text     "notes"
     t.string   "resource_state"
     t.index ["project_id"], name: "index_tasks_on_project_id", using: :btree
   end
