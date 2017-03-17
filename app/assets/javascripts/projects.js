ebc388517c6ced4a888ff00111470c10abb51a1b
Ewa <kocica@gmail.com>
2017-03-17 19:22:37 +0000

    Add javascript to ensure only one document field is added

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).on('turbolinks:load', function() {
+
+    $('[data-form-prepend]').click( function(e) {
+        e.preventDefault();
+        
+        var linkText = $(this).text() ;
+
+        if(linkText.indexOf("Document") != -1){
+          $(this).hide();
+        }
+    });
+
+});
