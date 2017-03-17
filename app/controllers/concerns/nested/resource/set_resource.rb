3f2d83644a947718d1167d4156efac283bce47a0
drakkensaer <shadowdrakkeen@gmail.com>
2017-03-17 23:22:36 +0000

    update

--- app/controllers/concerns/nested/resource/set_resource.rb
+++ app/controllers/concerns/nested/resource/set_resource.rb
@@ -5,6 +5,7 @@ module Nested::Resource::SetResource
         # This method is used to infer an object's class and it's ID by using common patterns among routes
         def set_resource
             route_array = request.fullpath.split('/')
+            puts route_array
             @route_resource = route_array[1]
             klass = @route_resource.singularize.capitalize.constantize
             object_id = route_array[2]
