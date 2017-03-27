33c237af8da7433a0193b9f40334b49972127209
Ewa <kocica@gmail.com>
2017-03-27 22:37:50 +0000

    Add devise_invitable integration

---
+++ app/controllers/concerns/resource/role/add_default_role.rb
@@ -0,0 +1,17 @@
+module Resource::Role::AddDefaultRole
+  
+  private
+  
+    # This method is used to find the note or note_attributes node and update the author to current_user
+    def set_author
+      model = controller_name.singularize.downcase
+      if model == "note"
+        params[model][:author] = current_user.name
+      elsif params[model].key? :notes_attributes
+        params[model][:notes_attributes].each do |note|
+          params[model][:notes_attributes][note][:author] = current_user.name
+          puts params[model][:notes_attributes][note][:author]
+        end
+      end
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324184119) do
+ActiveRecord::Schema.define(version: 20170327205159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,16 @@ ActiveRecord::Schema.define(version: 20170324184119) do
     t.string   "resource_state"
   end
 
+  create_table "resource_fields", force: :cascade do |t|
+    t.string   "data_key"
+    t.string   "data_value"
+    t.string   "fieldable_type"
+    t.integer  "fieldable_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["fieldable_type", "fieldable_id"], name: "index_resource_fields_on_fieldable_type_and_fieldable_id", using: :btree
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string   "name"
     t.string   "resource_type"
@@ -124,7 +134,18 @@ ActiveRecord::Schema.define(version: 20170324184119) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "phone"
+    t.string   "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer  "invitation_limit"
+    t.string   "invited_by_type"
+    t.integer  "invited_by_id"
+    t.integer  "invitations_count",      default: 0
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
+    t.index ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
+    t.index ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
