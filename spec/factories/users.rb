c5a54de7f333ee72c9c1d19fca976b3231d42424
Marty Kovach <tyedye105@yahoo.com>
2017-03-27 09:20:28 -0700

    update the seed file

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,12 @@
 FactoryGirl.define do
     factory :user do
+        first_name {Faker::Name}
+        last_name {Faker::Name}
+        phone "8888888888"
         email { Faker::Internet.email }
         password "password"
         password_confirmation "password"
-        
+
         factory :admin do
             after(:create) { |user| user.add_role(:admin) }
         end
