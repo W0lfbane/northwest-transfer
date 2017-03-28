c1b353f7bc9d2819d7f1c0a84f343a326d768ea7
Kevin Finley <kftwotwo@gmail.com>
2017-03-28 12:17:23 -0700

    will not create of author is nil

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -7,4 +7,9 @@ subject { FactoryGirl.create(:task) }
   it"has a valid factory" do
     expect( subject.notes.create(attributes_for(:note))).to be_valid
   end
+
+  it "not be valid if author is nil" do
+    note =  subject.notes.create(attributes_for(:note, author: nil))
+    expect( note ).to_not be_valid
+  end
 end
