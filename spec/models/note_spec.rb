568216609fe14401941e2547662e87995c1ad45f
drakkensaer <shadowdrakkeen@gmail.com>
2017-03-28 22:57:01 +0000

    update

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-describe Note do
+describe Note, type: :model do
+  subject { FactoryGirl.create(:task) }
 
-subject { FactoryGirl.create(:task) }
-
-  it"has a valid factory" do
+  it "has a valid factory" do
     expect( subject.notes.create(attributes_for(:note))).to be_valid
   end
 
