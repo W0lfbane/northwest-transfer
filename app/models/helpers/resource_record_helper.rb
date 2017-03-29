28c7a15070a9eaf2fc239a540c6b01bbc71c0297
drakkensaer <shadowdrakkeen@gmail.com>
2017-03-29 18:02:41 +0000

    update

--- app/models/helpers/resource_record_helper.rb
+++ app/models/helpers/resource_record_helper.rb
@@ -1,11 +1,42 @@
 module Helpers::ResourceRecordHelper
 
-  def next
-    self.class.where("id > ?", id).first  
+  # Remove this and call upon the module's methods to include instead to fix performance hit
+  def self.included(source)
+    define_class_methods(source)
   end
 
-  def previous
-    self.class.where("id < ?", id).last
+  def define_class_methods(source)
+    source.class_eval do 
+
+      source.attribute_names.each do |attribute|
+        define_method("next_by_" + attribute) do |user = nil|
+          if user
+            source.joins(:users).where("#{source.table_name}.#{attribute} > ?", self.public_send(attribute)).where(users: {id: user.id}).first
+          else
+            source.where("#{attribute} > ?", self.public_send(attribute)).first
+          end
+        end
+  
+        define_method("previous_by_" + attribute) do |user = nil|
+          if user
+            source.joins(:users).where("#{source.table_name}.#{attribute} < ?", self.public_send(attribute)).where(users: {id: user.id}).last
+          else
+            source.where("#{attribute} < ?", self.public_send(attribute)).last
+          end
+        end
+      end
+
+      def next(user = nil)
+        self.next_by_id(user)
+      end
+    
+      def previous(user = nil)
+        self.previous_by_id(user)
+      end
+
+    end
   end
+  
+  extend self
 
 end
