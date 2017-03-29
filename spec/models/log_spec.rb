17889b249bc8ed8e94a04719b0fd627e71394fbe
Kevin Finley <kftwotwo@gmail.com>
2017-03-29 10:20:44 -0700

    add specs for the model log

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Log, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Log, type: :model do
+  it "have a valid factory" do
+    expect(FactoryGirl.build(:log)).to be_valid
+  end
+
+  it "validate the prences of text field" do
+    expect(FactoryGirl.build(:log, text: nil)).not_to be_valid
+  end
 end
