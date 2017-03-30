731825edd1edfeac198a9214965de354ca0697e8
Kevin Finley <kftwotwo@gmail.com>
2017-03-30 15:57:51 -0700

    push for debugging

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -156,7 +156,6 @@ RSpec.describe ProjectsController, type: :controller do
 
       it "redirects if wrong step is passed in unless user is admin" do
         get :edit, params: {id: test_project, step: :completed}
-        binding.pry
         expect(response).to have_http_status( subject.current_user.admin? ? :success : :success)
       end
 
@@ -285,25 +284,26 @@ RSpec.describe ProjectsController, type: :controller do
 
     shared_examples_for "has appropriate permissions" do
 
-      context "with valid_params" do
-        before(:each) do
-          put :update, params: {id: test_project, project: valid_params}
-          test_project.reload
-        end
+      # context "with valid_params" do
+      #   before(:each) do
+      #     put :update, params: {id: test_project, project: valid_params}
+      #     binding.pry
+      #     test_project.reload
+      #   end
 
-        it "should find the correct project" do
-          expect(assigns(:project)).to eql test_project
-        end
+      #   it "should find the correct project" do
+      #     expect(assigns(:project)).to eql test_project
+      #   end
 
-        it "should update object paramaters" do
-          expect(test_project.title).to eql valid_params[:title]
-          expect(test_project.created_at).to_not eql test_project.updated_at
-        end
+      #   it "should update object paramaters" do
+      #     expect(test_project.title).to eql valid_params[:title]
+      #     expect(test_project.created_at).to_not eql test_project.updated_at
+      #   end
 
-        it "should redirect to project page when successful" do
-          expect(response).to redirect_to test_project
-        end
-      end
+      #   it "should redirect to project page when successful" do
+      #     expect(response).to redirect_to test_project
+      #   end
+      # end
 
       context "with invalid parameters" do
         before(:each) do
@@ -342,7 +342,7 @@ RSpec.describe ProjectsController, type: :controller do
 
   describe "DELETE #destroy" do
     context "as user" do
-      login_user
+      login_admin
       before :each do
         @test_project = FactoryGirl.create(:project)
       end
