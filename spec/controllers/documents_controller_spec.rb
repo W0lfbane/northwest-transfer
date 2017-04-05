78c8e63182366a9331410de4f5c2b1a751444d75
Kevin Finley <kftwotwo@gmail.com>
2017-04-05 11:49:46 -0700

    finsh the document controller for specs

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -2,32 +2,35 @@ require 'rails_helper'
 
 RSpec.describe DocumentsController, type: :controller do
 
-  let(:valid_attributes) { FactoryGirl.create(:document)  }
+  let(:valid_attributes) { FactoryGirl.attributes_for(:document)  }
+  let(:invalid_attributes) { FactoryGirl.attributes_for(:document, title: nil)  }
 
   describe "GET #show" do
 
     before :each do
       @project = FactoryGirl.create(:project)
-      @doc = FactoryGirl.create(:document)
+      @doc = @project.create_document(valid_attributes)
     end
 
     context "with admin" do
       login_admin
+
       it "recieves a 200 response" do
         get :show, params: {id: @doc.id, project_id: @project.id}
         expect(response.status).to eq(200)
       end
     end
 
-   context "with user" do
+    context "with user" do
       login_user
+
       it "recieves a 200 response" do
         get :show, params: {id: @doc.id, project_id: @project.id}
         expect(response.status).to eq(200)
       end
     end
 
-   context "with non-user" do
+    context "with non-user" do
       it "recieves a 200 response" do
         get :show, params: {id: @doc.id, project_id: @project.id}
         expect(response.status).to eq(302)
@@ -39,28 +42,29 @@ RSpec.describe DocumentsController, type: :controller do
 
     before :each do
       @project = FactoryGirl.create(:project)
-      @doc = FactoryGirl.create(:document)
     end
 
     context "with admin" do
       login_admin
+
       it "recieves a 200 response" do
-        get :new, params: {id: @doc.id, project_id: @project.id}
+        get :new, params: {project_id: @project.id}
         expect(response.status).to eq(200)
       end
     end
 
     context "with user" do
       login_user
+
       it "recievesa a 200 response" do
-        get :new, params: {id: @doc.id, project_id: @project.id}
+        get :new, params: {project_id: @project.id}
         expect(response.status).to eq(200)
       end
     end
 
     context "with non-user" do
       it "recievesa a 302 response" do
-        get :new, params: {id: @doc.id, project_id: @project.id}
+        get :new, params: {project_id: @project.id}
         expect(response.status).to eq(302)
       end
     end
@@ -70,22 +74,24 @@ RSpec.describe DocumentsController, type: :controller do
 
     before :each do
       @project = FactoryGirl.create(:project)
-      @doc = FactoryGirl.create(:document)
+      @doc = @project.create_document(valid_attributes)
     end
 
     context "with admin" do
       login_admin
-      it "assigns the requested note as @note" do
+
+      it "recievesa a 200 response" do
         get :edit, params: {id: @doc.id, project_id: @project.id}
-        expect(assigns(:note)).to eq(@note)
+        expect(response.status).to eq(200)
       end
     end
 
     context "with user" do
       login_user
-      it "assigns the requested note as @note" do
+
+      it "recievesa a 200 response" do
         get :edit, params: {id: @doc.id, project_id: @project.id}
-        expect(assigns(:note)).to eq(@note)
+        expect(response.status).to eq(200)
       end
     end
 
@@ -101,15 +107,21 @@ RSpec.describe DocumentsController, type: :controller do
 
     before :each do
       @project = FactoryGirl.create(:project)
-      @doc = FactoryGirl.create(:document)
     end
 
     context "with admin" do
       login_admin
-      it "creates a new Note" do
+
+      it "creates a new document" do
         expect {
-          post :create, params: {project_id: @project.id, document: valid_attributes}
+          post :create, params: {project_id: @project.id, document: valid_attributes }
         }.to change(Document, :count).by(1)
+      end
+
+      it "not create if title is nil" do
+        expect {
+          post :create, params: {project_id: @project.id, document: invalid_attributes }
+        }.to change(Document, :count).by(0)
       end
 
       it "assigns a newly created document as @doc" do
@@ -121,10 +133,17 @@ RSpec.describe DocumentsController, type: :controller do
 
     context "with user" do
       login_user
+
       it "creates a new Note" do
         expect {
           post :create, params: {project_id: @project.id, document: valid_attributes}
         }.to change(Document, :count).by(1)
+      end
+
+      it "not create if title is nil" do
+        expect {
+          post :create, params: {project_id: @project.id, document: invalid_attributes }
+        }.to change(Document, :count).by(0)
       end
 
       it "assigns a newly created document as @doc" do
@@ -136,54 +155,38 @@ RSpec.describe DocumentsController, type: :controller do
 
     context "with non-user" do
       it "gets redirected" do
-          post :create, params: {project_id: @project.id, document: valid_attributes}
+        post :create, params: {project_id: @project.id, document: valid_attributes}
         expect(response.status).to eq(302)
-      end
-
-      it "raises an error" do
-        expect {
-          post :create, params: {project_id: @project.id, document: valid_attributes}
-        }.to raise_error(Pundit::NotAuthorized)
       end
     end
   end
 
   describe "PUT #update" do
 
+    let(:new_attributes) { FactoryGirl.attributes_for(:document, title: 'new')}
+
     before :each do
       @project = FactoryGirl.create(:project)
-      @doc = FactoryGirl.create(:document)
+      @doc = @project.create_document(FactoryGirl.attributes_for(:document))
     end
 
     context "with admin" do
       login_admin
-      let(:new_attributes) { FactoryGirl.attributes_for(:document, title: 'new')}
 
       it "updates the requested document" do
         put :update, params: {id: @doc.id, document: new_attributes, project_id: @project.id}
         @doc.reload
         expect(@doc.title).to eq('new')
-      end
-
-      it "assigns the requested note as @doc" do
-        put :update, params: {id: @doc.id, document: new_attributes, project_id: @project.id}
-        expect(assigns(:docuement)).to eq(@doc)
       end
     end
 
     context "with user" do
       login_user
-      let(:new_attributes) { FactoryGirl.attributes_for(:document, title: 'new')}
 
       it "updates the requested document" do
         put :update, params: {id: @doc.id, document: new_attributes, project_id: @project.id}
         @doc.reload
         expect(@doc.title).to eq('new')
-      end
-
-      it "assigns the requested note as @doc" do
-        put :update, params: {id: @doc.id, document: new_attributes, project_id: @project.id}
-        expect(assigns(:docuement)).to eq(@doc)
       end
     end
   end
@@ -192,22 +195,23 @@ RSpec.describe DocumentsController, type: :controller do
 
     before :each do
       @project = FactoryGirl.create(:project)
-      @doc = FactoryGirl.create(:document)
+      @doc = @project.create_document(valid_attributes)
     end
 
     context "with admin" do
       login_admin
-      it "destroys the requested note" do
-        binding.pry
+
+      it "destroys the requested document" do
         expect {
           delete :destroy, params: {id: @doc.id, project_id: @project.id}
-        }.to change(Note, :count).by(-1)
+        }.to change(Document, :count).by(-1)
       end
     end
 
     context "with user" do
       login_user
-      it "destroys the requested note" do
+
+      it "destroys the requested docuemnt" do
         expect {
           delete :destroy, params: {id: @doc.id, project_id: @project.id}
         }.to raise_error(Pundit::NotAuthorizedError)
@@ -216,7 +220,7 @@ RSpec.describe DocumentsController, type: :controller do
 
     context "with non-user" do
       it "destroys the requested note" do
-          delete :destroy, params: {id: @doc.id, project_id: @project.id}
+        delete :destroy, params: {id: @doc.id, project_id: @project.id}
         expect(response.status).to eq(302)
       end
     end
