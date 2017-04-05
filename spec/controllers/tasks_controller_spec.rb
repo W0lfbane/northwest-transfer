339af8994fa6398ae2f4cf1a50baaebfc8d110bb
Marty Kovach <tyedye105@yahoo.com>
2017-04-05 12:28:52 -0700

    passing tasks_controller specs

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -20,13 +20,14 @@ require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
 
-  let(:valid_attributes) {FactoryGirl.attributes_for(:task, :project_id => @project.id)}
+  let(:valid_params) {FactoryGirl.attributes_for(:task, :project_id => @project.id)}
 
-  let(:invalid_attributes) { FactoryGirl.attributes_for(:task, :name => nil)}
+  let(:invalid_params) { FactoryGirl.attributes_for(:task, :name => nil)}
 
 
   before (:each) do
     @project = FactoryGirl.create(:project)
+      @task = Task.create! valid_params
   end
 
   # This should return the minimal set of attributes required to create a valid
@@ -34,72 +35,109 @@ RSpec.describe TasksController, type: :controller do
   # adjust the attributes here as well.
 
   describe "GET #index" do
-    before (:each) do
-      @task = Task.create! valid_attributes
-    end
+
+    context "non-registered user" do
+        it "will redirect to login page" do
+          get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
+          expect(response.status).to eq(302)
+        end
+      end
+
     context "user" do
       login_user
 
-    it " will have a succesful response" do
-      get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
-      expect(response.status).to eq(200)
+      it " will have a succesful response" do
+        get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
+        expect(response.status).to eq(200)
       end
 
-    it " will render the 'index' template " do
-      get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
-      expect(response).to render_template("index")
+      it " will render the :index template " do
+        get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
+        expect(response).to render_template("index")
       end
 
-    it "will assign the resource.tasks as @tasks " do
-      get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
-      expect(assigns(:tasks)).to eq([@task])
+      it "will assign the resource.tasks as @tasks " do
+        get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
+        expect(assigns(:tasks)).to eq([@task])
       end
     end
 
     context "admin" do
       login_admin
 
-    it " will have a successful response " do
-      get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
-      expect(response.status).to eq(200)
-      end
+      it " will have a successful response " do
+        get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
+        expect(response.status).to eq(200)
+        end
 
-    it " will render the 'index' template " do
-      get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
-      expect(response).to render_template("index")
-      end
+      it " will render the :index template " do
+        get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
+        expect(response).to render_template("index")
+        end
 
-    it "will assign the resource.tasks as @tasks " do
-      get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
-      expect(assigns(:tasks)).to eq([@task])
+      it "will assign the resource.tasks as @tasks " do
+        get :index, params: {:resource_controller => "projects", :resource_id => @project.id }
+        expect(assigns(:tasks)).to eq([@task])
+        end
       end
     end
-  end
 
   describe "GET #show" do
-    before (:each) do
-      @task = Task.create! valid_attributes
+
+
+    context "non-registered user" do
+      it "will redirect to login page" do
+      get :show, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
+        expect(response.status).to eq(302)
+      end
     end
+
     context "admin" do
       login_admin
-    it "will have a succesful response" do
-      get :show, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
-      expect(response.status).to eq(200)
+      it "will have a succesful response" do
+        get :show, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
+        expect(response.status).to eq(200)
       end
 
-    it "renders the :show template" do
-      get :show, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
-      expect(response).to render_template("show")
+      it "renders the :show template" do
+        get :show, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
+        expect(response).to render_template("show")
+        end
+
+      it "assigns the requested task as @task" do
+        get :show, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
+        expect(assigns(:task)).to eq(@task)
+        end
       end
 
-    it "assigns the requested task as @task" do
-      get :show, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
-      expect(assigns(:task)).to eq(@task)
+    context "user" do
+      login_user
+      it "will have a succesful response" do
+        get :show, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
+        expect(response.status).to eq(200)
+        end
+
+      it "renders the :show template" do
+        get :show, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
+        expect(response).to render_template("show")
+        end
+
+      it "assigns the requested task as @task" do
+        get :show, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
+        expect(assigns(:task)).to eq(@task)
+        end
       end
     end
-  end
 
   describe "GET #new" do
+
+    context "non-registered user" do
+      it "will redirect to login page" do
+      get :new, params: {:resource_controller => "projects", :resource_id => @project.id}
+        expect(response.status).to eq(302)
+      end
+    end
+
     context "admin" do
       login_admin
     it "have a succesful response" do
@@ -120,135 +158,156 @@ RSpec.describe TasksController, type: :controller do
   end
   #
   describe "GET #edit" do
-    before (:each) do
-      @task = Task.create! valid_attributes
-    end
+
     context "admin" do
       login_admin
 
-    it "have a succesful response" do
-      get :edit, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
-        expect(response.status).to eq(200)
-      end
+      it "have a succesful response" do
+        get :edit, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
+          expect(response.status).to eq(200)
+        end
 
-    it "assigns a new @task as @@task" do
-      get :edit, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
-      expect(response).to render_template("edit")
-      end
+      it "it renders the :edit template" do
+        get :edit, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
+        expect(response).to render_template("edit")
+        end
 
-    it "assigns a new task as @task" do
-      get :edit, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
-      expect(assigns(:task)).to eq(@task)
+      it "assigns a new task as @task" do
+        get :edit, params: {:resource_controller => "projects", :resource_id => @project.id, :id => @task.to_param}
+        expect(assigns(:task)).to eq(@task)
+        end
       end
     end
-  end
 
   describe "POST #create" do
     context "admin" do
       login_admin
 
-    context "with valid params" do
-      it "creates a new Task" do
-        expect {
-          post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: valid_attributes}
-        }.to change(Task, :count).by(1)
-      end
+        context "with valid params" do
+          it "creates a new Task" do
+            expect {
+              post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: valid_params}
+            }.to change(Task, :count).by(1)
+          end
 
-      it "creates a new Task" do
-        expect {
-          post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: valid_attributes}
-        }.to change(Task, :count).by(1)
-      end
+          it "creates a new Task" do
+            expect {
+              post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: valid_params}
+            }.to change(Task, :count).by(1)
+          end
 
-      it "assigns a newly created task as @task" do
-        post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: valid_attributes}
-        expect(assigns(:task)).to be_a(Task)
-        expect(assigns(:task)).to be_persisted
-      end
+          it "assigns a newly created task as @task" do
+            post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: valid_params}
+            expect(assigns(:task)).to be_a(Task)
+            expect(assigns(:task)).to be_persisted
+          end
 
-      it "redirects to the created task" do
-        post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: valid_attributes}
-        expect(response).to redirect_to(task_path(id: Task.last))
+          it "redirects to the created task" do
+            post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: valid_params}
+            expect(response).to redirect_to(task_path(id: Task.last))
+          end
+        end
+
+        context "with invalid params" do
+
+          it "will not increase the count of Tasks" do
+            expect {
+              post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: invalid_params}
+            }.to_not change(Task, :count)
+          end
+          it "assigns a newly created but unsaved task as @task" do
+            post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: invalid_params}
+            expect(assigns(:task)).to be_a_new(Task)
+          end
+
+          it "re-renders the 'new' template" do
+            post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: invalid_params}
+            expect(response).to render_template("new")
+          end
+        end
       end
     end
-    #
-    context "with invalid params" do
-
-      it "will not increase the count of Tasks" do
-        expect {
-          post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: invalid_attributes}
-        }.to_not change(Task, :count)
-      end
-      it "assigns a newly created but unsaved task as @task" do
-        post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: invalid_attributes}
-        expect(assigns(:task)).to be_a_new(Task)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, params: {:resource_controller => "projects", :resource_id => @project.id, task: invalid_attributes}
-        expect(response).to render_template("new")
-      end
-      end
-    end
-  end
 
   describe "PUT #update" do
+    let (:new_params) {FactoryGirl.attributes_for(:task, :name => "Updated Name")}
+
     context "admin" do
       login_admin
 
-    context "with valid params" do
-      let(:new_attributes) {FactoryGirl.attributes_for(:task,
-        :name => " Level the dinning room table.")
-      }
+        context "update with valid params" do
 
-      it "updates the requested task" do
-        task = Task.create! valid_attributes
-        put :update, params: {:resource_controller => "projects", :resource_id => @project.id, id: task.to_param, task: new_attributes}
-        expect(task.name).to eq new_attributes
+          it "will find the correct task" do
+            task = Task.create! valid_params
+            put :update, params: {:resource_controller => "projects", :resource_id => @project.id, id: task, task: new_params}
+            task.reload
+            expect(assigns(:task)).to eql(task)
+            end
+
+
+          it "will update the task" do
+            task = Task.create! valid_params
+            put :update, params: {:resource_controller => "projects", :resource_id => @project.id, id: task, task: new_params}
+            task.reload
+            expect(task.name).to eq new_params[:name]
+            expect(task.created_at).to_not eql(task.updated_at)
+            end
+
+          it "will redirect to task page on success" do
+            task = Task.create! valid_params
+            put :update, params: {:resource_controller => "projects", :resource_id => @project.id, id: task, task: new_params}
+            task.reload
+            expect(response).to redirect_to(task_path(id: task))
+          end
+        end
+
+        context "with invalid params" do
+
+          it "will find the correct task" do
+            task = Task.create! valid_params
+            put :update, params: {:resource_controller => "projects", :resource_id => @project.id, id: task, task: new_params}
+            task.reload
+            expect(assigns(:task)).to eql(task)
+            end
+
+          it "will not update parameters" do
+            task = Task.create! valid_params
+            put :update, params: {:resource_controller => "projects", :resource_id => @project.id, id: task, task: invalid_params}
+            task.reload
+            expect(task.name).to_not eq(invalid_params[:name])
+            expect(task.created_at).to eql(task.updated_at)
+        end
+          it "will re-render edit page" do
+            task = Task.create! valid_params
+            put :update, params: {:resource_controller => "projects", :resource_id => @project.id, id: task, task: invalid_params}
+            task.reload
+            expect(response).to render_template ("edit")
+          end
+        end
       end
+    end
 
-    #   it "assigns the requested task as @task" do
-    #     task = Task.create! valid_attributes
-    #     put :update, params: {id: task.to_param, task: valid_attributes}, session: valid_session
-    #     expect(assigns(:task)).to eq(task)
-    #   end
-    #
-    #   it "redirects to the task" do
-    #     task = Task.create! valid_attributes
-    #     put :update, params: {id: task.to_param, task: valid_attributes}, session: valid_session
-    #     expect(response).to redirect_to(task)
-    #   end
-    # end
-    #
-    # context "with invalid params" do
-    #   it "assigns the task as @task" do
-    #     task = Task.create! valid_attributes
-    #     put :update, params: {id: task.to_param, task: invalid_attributes}, session: valid_session
-    #     expect(assigns(:task)).to eq(task)
-    #   end
-    #
-    #   it "re-renders the 'edit' template" do
-    #     task = Task.create! valid_attributes
-    #     put :update, params: {id: task.to_param, task: invalid_attributes}, session: valid_session
-    #     expect(response).to render_template("edit")
-    #   end
+  describe "DELETE #destroy" do
+    context "as admin" do
+      login_admin
+
+      it "will find the correct task" do
+        task = Task.create! valid_params
+        delete :destroy, params: {:resource_controller => "projects", :resource_id => @project.id, id: task}
+        expect(assigns(:task)).to eql(task)
+        end
+
+      it "will delete the task" do
+        task = Task.create! valid_params
+        expect {
+          delete :destroy, params: {:resource_controller => "projects", :resource_id => @project.id, id:task}
+        }.to change(Task, :count).by(-1)
+        end
+
+      it "will redirect to tasks page" do
+        task = Task.create! valid_params
+        delete :destroy, params: {:resource_controller => "projects", :resource_id => @project.id, id: task}
+        expect(response).to redirect_to(:action => :index)
       end
     end
   end
-  #
-  # describe "DELETE #destroy" do
-  #   it "destroys the requested task" do
-  #     task = Task.create! valid_attributes
-  #     expect {
-  #       delete :destroy, params: {id: task.to_param}, session: valid_session
-  #     }.to change(Task, :count).by(-1)
-  #   end
-  #
-  #   it "redirects to the tasks list" do
-  #     task = Task.create! valid_attributes
-  #     delete :destroy, params: {id: task.to_param}, session: valid_session
-  #     expect(response).to redirect_to(tasks_url)
-  #   end
-  # end
-
 end
