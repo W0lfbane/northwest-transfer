db4fe3c48b80fd5b82f4ffb034327fa885ac704c
Marty Kovach <tyedye105@yahoo.com>
2017-04-17 15:32:49 -0700

    working crud for notes carosuel.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -63,7 +63,7 @@ class NotesController < ApplicationController
   def destroy
     @note.destroy
     respond_to do |format|
-      format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
+      format.html { redirect_to project_path(@note.loggable_id), notice: 'Note was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
