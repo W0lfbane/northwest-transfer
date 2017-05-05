29745bf676f3240787a2d93d28ddc0771112e35e
drakkensaer <shadowdrakkeen@gmail.com>
2017-05-05 20:46:16 +0000

    update

--- app/helpers/modules/role_form_fields.rb
+++ app/helpers/modules/role_form_fields.rb
@@ -1,13 +1,7 @@
 module Modules::RoleFormFields
-    def role_fields(f, resource, target_resource = nil)
-        target_resource ||= resource.class.new
-
-        f.collection_check_boxes(:role_ids, 
-                                resource.find_roles(target_resource.persisted? ? target_resource.class.name : nil, target_resource.id), 
-                                :id, 
-                                :name) do |field|
+    def role_fields(f, collection)
+        f.collection_check_boxes(:role_ids, collection, :id, :name) do |field|
             content_tag :div, field.label { field.check_box + field.text }, class: 'checkbox'
         end
-
     end
 end
